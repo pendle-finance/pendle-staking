381bac7f665d548f43549722fd02eb5d86c1888e
vicom <vunguyenvyvp@gmail.com>
2021-06-11 21:03:25 +0700

    Add events and return values

--- contracts/SingleStaking.sol
+++ contracts/SingleStaking.sol
@@ -7,6 +7,9 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./SingleStakingManager.sol";
 
 contract SingleStaking {
+    event Enter(address user, uint256 pendleAmount, uint256 shares);
+    event Leave(address user, uint256 pendleAmount, uint256 shares);
+
     using SafeMath for uint256;
     IERC20 public immutable rewardToken;
     SingleStakingManager public immutable stakingManager;
@@ -19,7 +22,7 @@ contract SingleStaking {
     }
 
     // Locks Pendle, update the user's shares (non-transferable)
-    function enter(uint256 _amount) public {
+    function enter(uint256 _amount) public returns (uint256 sharesToMint) {
         // Before doing anything, get the unclaimed rewards first
         stakingManager.distributeRewards();
         // Gets the amount of Pendle locked in the contract
@@ -27,28 +30,31 @@ contract SingleStaking {
         // Gets the amount of shares in existence
         uint256 totalShares = totalSupply;
         // If no shares exists, mint it 1:1 to the amount put in
+
         if (totalShares == 0 || totalRewardToken == 0) {
-            _mint(msg.sender, _amount);
+            sharesToMint = _amount;
         }
         // Calculate and mint the amount of shares the Pendle is worth. The ratio will change overtime, as shares is burned/minted and Pendle distributed to this contract
         else {
-            uint256 what = _amount.mul(totalShares).div(totalRewardToken);
-            _mint(msg.sender, what);
+            sharesToMint = _amount.mul(totalShares).div(totalRewardToken);
         }
+        _mint(msg.sender, sharesToMint);
         // Lock the Pendle in the contract
         rewardToken.transferFrom(msg.sender, address(this), _amount);
+        emit Enter(msg.sender, _amount, sharesToMint);
     }
 
     // Unlocks the staked + gained Pendle and burns shares
-    function leave(uint256 _share) public {
+    function leave(uint256 _share) public returns (uint256 rewards) {
         // Before doing anything, get the unclaimed rewards first
         stakingManager.distributeRewards();
         // Gets the amount of shares in existence
         uint256 totalShares = totalSupply;
         // Calculates the amount of Pendle the shares is worth
-        uint256 what = _share.mul(rewardToken.balanceOf(address(this))).div(totalShares);
+        rewards = _share.mul(rewardToken.balanceOf(address(this))).div(totalShares);
         _burn(msg.sender, _share);
-        rewardToken.transfer(msg.sender, what);
+        rewardToken.transfer(msg.sender, rewards);
+        emit Leave(msg.sender, rewards, _share);
     }
 
     function _mint(address user, uint256 amount) internal {
